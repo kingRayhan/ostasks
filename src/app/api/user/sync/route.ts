ea479e778c48eb6f50eaf2be0bf1053ac1291c60
kingrayhan <rayhan095@gmail.com>
2025-01-03 00:37:54 +0600

    user sync webhook api

--- src/app/api/user/sync/route.ts
+++ src/app/api/user/sync/route.ts
@@ -7,46 +7,50 @@ import { Webhook } from "svix";
 import { eq } from "drizzle-orm";
 
 export async function POST(req: Request) {
-  const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
-  if (!WEBHOOK_SECRET) throw new Error("Missing CLERK_WEBHOOK_SECRET");
+  // const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
+  // if (!WEBHOOK_SECRET) throw new Error("Missing CLERK_WEBHOOK_SECRET");
   const _clerkClient = await clerkClient();
 
-  const headerPayload = await headers();
-  const svix_id = headerPayload.get("svix-id");
-  const svix_timestamp = headerPayload.get("svix-timestamp");
-  const svix_signature = headerPayload.get("svix-signature");
-
-  if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response("Missing svix headers", { status: 400 });
-  }
-
   const payload = await req.json();
-  const body = JSON.stringify(payload);
 
-  const wh = new Webhook(WEBHOOK_SECRET);
+  console.log(JSON.stringify(payload, null, 2));
+
+  // const headerPayload = await headers();
+  // const payload = await req.json();
+  // const body = JSON.stringify(payload);
+
+  // const svix_id = headerPayload.get("svix-id");
+  // const svix_timestamp = headerPayload.get("svix-timestamp");
+  // const svix_signature = headerPayload.get("svix-signature");
+
+  // if (!svix_id || !svix_timestamp || !svix_signature) {
+  //   return new Response("Missing svix headers", { status: 400 });
+  // }
+
+  // const wh = new Webhook(WEBHOOK_SECRET);
 
   try {
-    const evt = wh.verify(body, {
-      "svix-id": svix_id,
-      "svix-timestamp": svix_timestamp,
-      "svix-signature": svix_signature,
-    }) as WebhookEvent;
+    // const evt = wh.verify(body, {
+    //   "svix-id": svix_id,
+    //   "svix-timestamp": svix_timestamp,
+    //   "svix-signature": svix_signature,
+    // }) as WebhookEvent;
 
-    switch (evt.type) {
+    switch (payload.type) {
       case "user.created":
         const createdUser = await db
           .insert(users)
           .values({
-            auth_uid: evt.data.id,
-            email: evt.data.email_addresses[0]?.email_address,
-            firstName: evt.data.first_name,
-            lastName: evt.data.last_name,
-            username: evt.data.username,
-            imageUrl: evt.data.image_url,
+            auth_uid: payload.data.id,
+            email: payload.data.email_addresses[0]?.email_address,
+            firstName: payload.data.first_name,
+            lastName: payload.data.last_name,
+            username: payload.data.username,
+            imageUrl: payload.data.image_url,
           })
           .returning({ id: users.id });
 
-        await _clerkClient.users.updateUser(evt.data.id, {
+        await _clerkClient.users.updateUser(payload.data.id, {
           publicMetadata: {
             dbUserId: createdUser?.[0].id,
           },
@@ -57,18 +61,20 @@ export async function POST(req: Request) {
         await db
           .update(users)
           .set({
-            email: evt.data.email_addresses[0]?.email_address,
-            firstName: evt.data.first_name,
-            lastName: evt.data.last_name,
-            username: evt.data.username,
-            imageUrl: evt.data.image_url,
+            email: payload.data.email_addresses[0]?.email_address,
+            firstName: payload.data.first_name,
+            lastName: payload.data.last_name,
+            username: payload.data.username,
+            imageUrl: payload.data.image_url,
             lastSynced: new Date(),
           })
-          .where(eq(users.auth_uid, evt.data.id));
+          .where(eq(users.auth_uid, payload.data.id));
         break;
 
       case "user.deleted":
-        await db.delete(users).where(eq(users.auth_uid, evt.data.id as any));
+        await db
+          .delete(users)
+          .where(eq(users.auth_uid, payload.data.id as any));
         break;
     }
 
